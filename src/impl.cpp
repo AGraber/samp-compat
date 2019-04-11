7b16130082c197cd016cc5ae4a48487dd3fe47e9
Romz24 <romz24@mail.ru>
2019-04-11 22:15:12 +0300

    Fix actor bug (#3)
    * Fix actor bug
    
    Fix this bug: https://github.com/AGraber/samp-dl-compat/issues/17
    
    * fix variable type & added fix for RPC_03DL

--- src/impl.cpp
+++ src/impl.cpp
@@ -205,6 +205,7 @@ public:
 						Z,
 						Angle,
 						health;
+					uint8_t bInvulnerable;
 
 					bs.Read(wActorID);
 					bs.Read(SkinID);
@@ -213,6 +214,7 @@ public:
 					bs.Read(Z);
 					bs.Read(Angle);
 					bs.Read(health);
+					bs.Read(bInvulnerable);
 
 					bs.Reset();
 
@@ -224,6 +226,7 @@ public:
 					bs.Write(Z);
 					bs.Write(Angle);
 					bs.Write(health);
+					bs.Write(bInvulnerable);
 
 					return pfn__RakNet__RPC(ppRakServer, uniqueID, &bs, priority, reliability, orderingChannel, playerId, broadcast, shiftTimestamp);
 				}
@@ -420,6 +423,7 @@ public:
 						Z,
 						Angle,
 						health;
+					uint8_t bInvulnerable;
 
 					bs.Read(wActorID);
 					bs.Read(SkinID);
@@ -429,6 +433,7 @@ public:
 					bs.Read(Z);
 					bs.Read(Angle);
 					bs.Read(health);
+					bs.Read(bInvulnerable);
 
 					bs.Reset();
 
@@ -439,6 +444,7 @@ public:
 					bs.Write(Z);
 					bs.Write(Angle);
 					bs.Write(health);
+					bs.Write(bInvulnerable);
 
 					return pfn__RakNet__RPC(ppRakServer, uniqueID, &bs, priority, reliability, orderingChannel, playerId, broadcast, shiftTimestamp);
 				}
